c16ef861d5340b55b379612b054912e4716a10be
yunob01 <s111219038@stu.ntue.edu.tw>
2025-04-18 12:34:23 +0800

    加入標題動畫

--- src/components/MainVisual.jsx
+++ src/components/MainVisual.jsx
@@ -1,8 +1,19 @@
 import { Link } from 'react-router';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 function MainVisual({ title, slogan1, slogan2Line1, slogan2Line2 }) {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"]
+  });
+
+  const opacity = useTransform(scrollYProgress, [0, 0.3], [1, 0]);
+
   return (
-    <div className="relative h-screen">
+    <div className="relative h-screen" ref={ref}>
       <img
         src="/images/home-coffeebeans.jpg"
         alt="coffee beans background"
@@ -11,8 +22,16 @@ function MainVisual({ title, slogan1, slogan2Line1, slogan2Line2 }) {
 
       <div className="absolute inset-0 bg-gradient-to-r from-black/30 to-transparent z-10"></div>
 
-      <div className="px-6 lg:px-12 pt-24 z-20 relative">
-        <div className="max-w-2xl text-left">
+      <motion.div
+        className="px-6 lg:px-12 pt-24 z-20 relative"
+        style={{ opacity }}
+      >
+        <motion.div
+          className="max-w-2xl text-left"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
           <Link to="/">
             <h2 className="text-white text-2xl sm:text-3xl md:text-4xl font-bold font-serif">
               {title}
@@ -27,8 +46,8 @@ function MainVisual({ title, slogan1, slogan2Line1, slogan2Line2 }) {
               {slogan2Line2}
             </p>
           </Link>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
